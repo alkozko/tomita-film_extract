662e97831acbf60396d2cde8f3daa11013bef3d9
allkozko <alkozko@gmail.com>
2017-10-30 12:53:01 +0500

    Извлекаем названия в кавычках

--- film.cxx
+++ film.cxx
@@ -5,4 +5,4 @@ Title -> Word<h-reg1, quoted>;
 Title -> Word<h-reg1, l-quoted, ~r-quoted> Word<~l-quoted, r-quoted>;
 Title -> Word<h-reg1, l-quoted, ~r-quoted> AnyWord<~r-quoted>+ Word<~l-quoted, r-quoted>;
 
-S -> Title;
+S -> Title interp(Film.Name);
