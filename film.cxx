2af89ddd498072dca118086b78e4e639d7953bc9
allkozko <alkozko@gmail.com>
2017-10-30 12:02:47 +0500

    Извлекаем названия в кавычках

--- film.cxx
+++ film.cxx
@@ -1,4 +1,8 @@
 #encoding "utf-8"
 #GRAMMAR_ROOT S
 
-S -> Adj Noun;
+Title -> Word<h-reg1, quoted>;
+Title -> Word<h-reg1, l-quoted, ~r-quoted> Word<~l-quoted, r-quoted>;
+Title -> Word<h-reg1, l-quoted, ~r-quoted> AnyWord<~r-quoted>+ Word<~l-quoted, r-quoted>;
+
+S -> Title;
