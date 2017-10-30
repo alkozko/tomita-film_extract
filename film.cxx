4f2154b4263076a962d944714c67e401176ebb1c
allkozko <alkozko@gmail.com>
2017-10-30 13:45:07 +0500

    Жанр - это тоже дескриптор

--- film.cxx
+++ film.cxx
@@ -5,7 +5,9 @@ Title -> Word<h-reg1, quoted>;
 Title -> Word<h-reg1, l-quoted, ~r-quoted> Word<~l-quoted, r-quoted>;
 Title -> Word<h-reg1, l-quoted, ~r-quoted> AnyWord<~r-quoted>+ Word<~l-quoted, r-quoted>;
 
+Genre -> Word<kwtype="жанр">;
 FilmDescriptor -> Adj* Word<wff=/фильм(-.+)?/>;
+FilmDescriptor -> Adj* Genre;
 
 S -> Title interp(Film.Name::not_norm) Hyphen* FilmDescriptor;
 S -> FilmDescriptor Title interp(Film.Name::not_norm);
