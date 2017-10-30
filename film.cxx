15d9d78f6e55780f400c7896e3fa13ad05621c30
allkozko <alkozko@gmail.com>
2017-10-30 15:41:55 +0500

    Исправляем третий фильм

--- film.cxx
+++ film.cxx
@@ -12,7 +12,7 @@ Title -> Word<h-reg1, quoted>;
 Title -> Word<h-reg1, l-quoted, ~r-quoted> Word<~l-quoted, r-quoted>;
 Title -> Word<h-reg1, l-quoted, ~r-quoted> AnyWord<~r-quoted>+ Word<~l-quoted, r-quoted>;
 
-GenreDescr -> Word<kwtype="жанр"> | Word<wff=/фильм-.+/> | Adj "фильм";
+GenreDescr -> Word<kwtype="жанр"> | Word<wff=/фильм-.+/> | Adj<gram='A'> "фильм";
 Genre -> Adj* GenreDescr;
 FilmDescriptor -> Genre interp(Film.Genre) | "фильм";
 
