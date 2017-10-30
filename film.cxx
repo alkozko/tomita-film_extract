ddeda942745fc3b0fe4be59dd83fa54f772a8168
allkozko <alkozko@gmail.com>
2017-10-30 15:37:57 +0500

    Даты премьер

--- film.cxx
+++ film.cxx
@@ -19,6 +19,13 @@ FilmDescriptor -> Genre interp(Film.Genre) | "фильм";
 FilmTitle -> Title interp(Film.Name::not_norm);
 FilmTitle -> FilmTitle "и" FilmTitle;
 
+
+Premiere -> "премьера" | "выход" "в" (Adj) "прокат";
+// Цепочки дат
+PlacePrep -> "в" | "на";
+Place -> PlacePrep (Word) Noun;
+S -> Premiere (Word) Verb (Place) (Prep) Date interp (Film.Date::not_norm);
+
 Name -> Word<h-reg1, ~fw, nc-agr[1]> Word<h-reg1, nc-agr[1]>*;
 
 //Хак для звездных войн
@@ -28,5 +35,5 @@ Director -> Name<gram="род"> interp (Film.Director);
 Director -> Comma 'снять' <gram="прич"> (NP) Name<gram="твор"> interp (Film.Director);
 Director -> 'режиссер'<gram="род"> Name interp (Film.Director);
 
-S -> FilmTitle Hyphen* FilmDescriptor (Date interp (Film.Date)) (SW_HACK) (Director);
+S -> FilmTitle Hyphen* FilmDescriptor (Date interp (Film.Date::not_norm)) (SW_HACK) (Director);
 S -> FilmDescriptor (Director) FilmTitle;
