7c515afc062fbf1ca1471ed0b477f0327907d083
allkozko <alkozko@gmail.com>
2017-10-30 12:56:17 +0500

    Убираем нормализацию названия

--- film.cxx
+++ film.cxx
@@ -5,4 +5,4 @@ Title -> Word<h-reg1, quoted>;
 Title -> Word<h-reg1, l-quoted, ~r-quoted> Word<~l-quoted, r-quoted>;
 Title -> Word<h-reg1, l-quoted, ~r-quoted> AnyWord<~r-quoted>+ Word<~l-quoted, r-quoted>;
 
-S -> Title interp(Film.Name);
+S -> Title interp(Film.Name::not_norm);
